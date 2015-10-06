29c28662350157f40e51ee78095755b413dd8976
Remy <fevrer@localhost.localdomain>
2015-10-06 19:18:31 +0200

    suppresions fichier inutile

--- sources/JeuQuestions.java
+++
@@ -1,9 +0,0 @@
-
-
-class JeuQuestions {
-    public static void main(String[] args) {
-	NoeudArbre aboie = new NoeudArbre("Est-ce qu’il aboie ?",new NoeudArbre("un cheval"),new NoeudArbre("un chien"));
-					  NoeudArbre noeud = new NoeudArbre("Est-ce un mammifère ?",new NoeudArbre("un crocodile"),aboie);
-	System.out.println(noeud.toString());
-    }	
-}
