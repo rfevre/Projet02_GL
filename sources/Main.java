2719557fccccc56f014c9011a3c1c704642b66fa
LeJoyeuxCodeur <vergauwb@meleze09.iut-infobio.priv.univ-lille1.fr>
2015-10-12 13:52:27 +0200

    toto

--- sources/Main.java
+++ sources/Main.java
@@ -1,13 +1,11 @@
 public class Main{
-
-
      /**
      * Fonction principal qui construit l'arbre, et lance le jeu
      */
     public static void main(String[] args) {
-		NoeudArbre sousBranche,arbre;
-		String definir="";
-		char reponse='n';
+		Arbre arbre;
+		String definir = "";
+		char reponse = 'n';
 
 		do {
 			System.out.println();
@@ -19,15 +17,17 @@ public class Main{
 			//Si pas d'arguments
 			if (args.length == 0) {
 				//On créer un abre par défaut
-				sousBranche = new NoeudArbre("Est-ce qu’il aboie ?", new NoeudArbre("un cheval"), new NoeudArbre("un chien"));
-				arbre = new NoeudArbre("Est-ce un mammifère ?", new NoeudArbre("un crocodile"), sousBranche);
+				
+				String[] values = new String[]{"Est-ce un mammifère ?", "un crocodile", "Est-ce qu’il aboie ?", "Un cheval", "Un chien"};
+				arbre = new Arbre(values, 0);
+				
 				//On lance la recherche d'un animal
 				arbre.rechercherAnimal(); 
 			}
 			else {
-				//Si l'argument d'indice 0 est égale à "--definir" 
+				//Si l'argument d'indice 0 est égal à "--definir" 
 				if (args[0].equals("--definir")){                           
-					arbre = NoeudArbre.creerArbre(args);         
+					arbre = new Arbre(args, 2);         
 					//On lance la recherche automatique d'un animal                     
 					definir = arbre.definir(args[1]);            
 					
@@ -37,9 +37,10 @@ public class Main{
 					else
 						System.out.println("Animal non trouvé");
 				}
-				 //Si pas d'argument spécial à l'indice 0
+				// Si pas d'argument spécial à l'indice 0
 				else {                                                     
-					arbre = NoeudArbre.creerArbre(args,0);
+					arbre = new Arbre(args, 0);
+					System.out.println(arbre);
 					//On lance la recherche d'un animal
 					arbre.rechercherAnimal();                               
 				}
@@ -48,7 +49,7 @@ public class Main{
 			System.out.println("\n------------------------------------------------");
 			System.out.println("-                  Arbre Final                 -");
 			System.out.println("------------------------------------------------ \n");
-			System.out.println(arbre.toString());
+			System.out.println(arbre);
 			System.out.println("\n Voulez vous quitter ? (o/n)");
 			reponse=Clavier.readChar();
 		} while(reponse !='o');
